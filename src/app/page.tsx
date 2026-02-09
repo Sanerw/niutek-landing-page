5196ce098b94d20161eb436a5104cf84b04875ab
sanerw4 <sanerw4@gmail.com>
2026-02-09 22:37:06 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-hidden bg-zinc-50">
+    <div className="h-dvh overflow-hidden bg-zinc-50">
       <Background />
       <div className="relative z-1 flex items-center gap-8 flex-col pt-24 pb-16 px-8">
         {/* <Header /> */}
