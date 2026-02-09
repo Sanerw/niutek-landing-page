a825c759698b0b1420b9af23a3bda1d3a43ccb37
sanerw4 <sanerw4@gmail.com>
2026-02-09 20:41:16 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Background from "@/components/Background";
+import Envelope from "@/components/Envelope";
+import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <Background />
+      <div className="relative z-1 flex items-center gap-8 flex-col py-16 px-8">
+        <Header />
+        <Envelope />
+      </div>
     </div>
   );
 }
--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const Envelope = () => {
-  return <div>Envelope</div>;
+  return (
+    <div>
+      <Image
+        src="/images/envelope2.png"
+        alt="Envelope"
+        width={490}
+        height={580}
+      />
+    </div>
+  );
 };
 
 export default Envelope;
