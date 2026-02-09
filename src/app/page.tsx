574fcfca6e970732b451d3bdc0aa057b86d50391
sanerw4 <sanerw4@gmail.com>
2026-02-09 21:07:03 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <Background />
-      <div className="relative z-1 flex items-center gap-8 flex-col py-16 px-8">
+      <div className="relative z-1 flex items-center gap-8 flex-col pt-40 pb-16 px-8">
         <Header />
         <Envelope />
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
+import Image from "next/image";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="absolute top-8 left-auto right-auto">
+      <Image src="/images/header.png" alt="Header" width={400} height={100} />
+    </div>
+  );
 };
 
 export default Header;
