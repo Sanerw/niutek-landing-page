3819b7fab01bb38b93f2edaa3768b3ba9d2f6d72
sanerw4 <sanerw4@gmail.com>
2026-02-09 22:10:52 +0100

    .

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -22,7 +22,7 @@ const Envelope = () => {
     setIsTyping(true);
 
     const type = () => {
-      if (i < messages[index].length) {
+      if (i < messages[index].length - 1) {
         setDisplayedText((prev) => prev + messages[index][i]);
         i++;
         timeout = setTimeout(type, 40); // prędkość pisania
@@ -45,7 +45,7 @@ const Envelope = () => {
   return (
     <div className="relative">
       <Image
-        src="/images/envelope2.png"
+        src="/images/envelope3.png"
         alt="Envelope"
         width={490}
         height={580}
@@ -64,7 +64,8 @@ const Envelope = () => {
       <button
         onClick={handleClick}
         disabled={isTyping}
-        className="absolute bottom-7 left-1/2 -translate-x-1/2 bg-[#5c3436] text-white px-4 py-2 rounded disabled:opacity-50"
+        // className="absolute bottom-7 left-1/2 -translate-x-1/2 bg-[#e04852] text-white px-4 py-2 rounded-xl disabled:opacity-50"
+        className="absolute bottom-7 left-1/2 -translate-x-1/2 opacity-0"
       >
         Kliknij mnie !!
       </button>
